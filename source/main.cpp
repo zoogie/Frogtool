cf3929c3ce59972a25c12935162d45f567de2c2e
zoogie <secretAgentCokey@gmail.com>
2019-11-07 20:17:04 -0600

    v2.3
    Change firmware version error to warning because we can fix firm0 write mistakes easily with a dry update.

--- source/main.cpp
+++ source/main.cpp
@@ -79,7 +79,7 @@ Result export_tad(u64 tid, u8 op, u8 *workbuf, const char *ext){  //export is a 
 
 Result menuUpdate(int cursor, int showinfo){
 	consoleClear();
-	printf("%sFrogtool v2.2 - zoogie & jason0597%s\n\n", green, white);
+	printf("%sFrogtool v2.3 - zoogie & jason0597%s\n\n", green, white);
 	char menu[menu_size][128] = {
 		"INJECT  patched          DS Download Play",
 		"BOOT    patched          DS Download Play",
@@ -317,8 +317,8 @@ int main(int argc, char* argv[])
 	const char *ppm="/private/ds/app/4B47554A/001/T00031_1038C2A757B77_000.ppm";
 	Result res;
 	
-	u32 kver = osGetKernelVersion();   //the current recommended frogminer guide requires firm 11.8 so we will enforce that here. if a surprise firm drops and native firm changes, this will safeguard users immediately
-	if(kver != 0x02370000){
+	u32 kver = osGetKernelVersion();   //the current recommended frogminer guide requires firm 11.12 so we will kinda suggest that here. if a surprise firm drops and native firm changes, this will safeguard users immediately
+	if(kver != 0x02380000){
 		wrongfirmware=1;
 	}	
 											//almost all versions of luma a9lh that can run on 11.9+ have a custom svc called svcGetCFWinfo that places the text "LUMA" at the beginning of a 16 byte buffer arg1
@@ -383,6 +383,7 @@ int main(int argc, char* argv[])
 	tid = 0x00048005484E4441;   //dlp
 	memcpy(fb, superfrog_bin, superfrog_bin_size);
 	menuUpdate(cursor, showinfo);
+	if(wrongfirmware) printf("\n\nWARNING!!\nYou are not on the expected firmware!\n(firm 11.12.0-X)\n");
 	
 	while (aptMainLoop())
 	{
@@ -396,7 +397,7 @@ int main(int argc, char* argv[])
 		if(kDown & KEY_A){
 			switch(cursor){
 				case 0: if(havecfw) {printf("You already have CFW! %d\n\n", havecfw); break;}
-						if(wrongfirmware) {printf("You are not on the correct firmware!\n\n"); break;}
+						//if(wrongfirmware) {printf("You are not on the correct firmware!\n\n"); break;}
 						export_tad(tid, op, buf, ".bin"); if(doStuff()) break;
 				        import_tad(tid, op, buf, ".bin.patched"); break;
 				case 1: //if(havecfw) {printf("You already have CFW!\n\n"); break;}
